4f8c568e0192138a2382efa37cea91159503483e
supermazlum <mazlum.superc@gmail.com>
2023-03-04 00:42:57 +0100

    version_1_2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,3 @@
-console.log("test");
+// console.log("test");
+
+// BONUS: Füge eine Seite hinzu, wo der User sich für einen Newsletter anmelden kann. Lass Dir die eingetragenen Daten in der Konsole ausgeben.
