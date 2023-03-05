1dac5ecab024871032dccff8cc73223d32989720
supermazlum <mazlum.superc@gmail.com>
2023-03-05 20:59:06 +0100

    preModal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-console.log("test");
+// console.log("test");
 
 // BONUS: Füge eine Seite hinzu, wo der User sich für einen Newsletter anmelden kann. Lass Dir die eingetragenen Daten in der Konsole ausgeben.
 
@@ -7,3 +7,11 @@ function mailcheck() {
     let usermail = document.getElementById('usermail').value;
     console.log(usermail);
 }
+
+function popUp() {
+    alert("Successfully subscribed. Thank you!");
+}
+
+// if(confirm("Ok!")) {
+//     console.log("Done!")
+// }
