79ee4b46ac37124e5ceea9b4c5233c8a46a53363
supermazlum <mazlum.superc@gmail.com>
2023-03-05 17:46:56 +0100

    design-shopJS1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,9 @@
 console.log("test");
 
 // BONUS: Füge eine Seite hinzu, wo der User sich für einen Newsletter anmelden kann. Lass Dir die eingetragenen Daten in der Konsole ausgeben.
+
+
+function mailcheck() {
+    let usermail = document.getElementById('usermail').value;
+    console.log(usermail);
+}
